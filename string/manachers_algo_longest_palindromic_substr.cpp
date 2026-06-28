579f300325426386f2056ed199261282955c4649
agdevtrack20 <agdevtrack@gmail.com>
2026-06-28 22:36:40 +0530

    longest palindromic string

---
+++ string/manachers_algo_longest_palindromic_substr.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        if (s.empty()) return "";
+
+        // Transform string
+        string t = "^";
+        for (char c : s) {
+            t += "#";
+            t += c;
+        }
+        t += "#$";
+
+        int n = t.size();
+        vector<int> P(n, 0);
+
+        int center = 0;
+        int right = 0;
+
+        for (int i = 1; i < n - 1; i++) {
+
+            int mirror = 2 * center - i;
+
+            if (i < right)
+                P[i] = min(right - i, P[mirror]);
+
+            while (t[i + P[i] + 1] == t[i - P[i] - 1])
+                P[i]++;
+
+            if (i + P[i] > right) {
+                center = i;
+                right = i + P[i];
+            }
+        }
+
+        int maxLen = 0;
+        int centerIndex = 0;
+
+        for (int i = 1; i < n - 1; i++) {
+            if (P[i] > maxLen) {
+                maxLen = P[i];
+                centerIndex = i;
+            }
+        }
+
+        int start = (centerIndex - maxLen) / 2;
+
+        return s.substr(start, maxLen);
+    }
+};
